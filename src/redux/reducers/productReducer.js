cc8c28ac2943cefdb40d9d07388b19d09e85d216
Johnny Wright <johnnywrightiv@gmail.com>
2023-01-19 21:30:46 -0600

    setup store, reducers, actions, and provider

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,21 @@
+import { ActionTypes } from "../constants/action-types";
+
+
+const initialState = {
+  products:[{
+    id: 1,
+    title: "Johnny",
+    category: "Programmer"
+  }]
+}
+
+export const productReducer = (state = initialState, {type, payload}) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+}
+
+// SELECTED_PRODUCT
