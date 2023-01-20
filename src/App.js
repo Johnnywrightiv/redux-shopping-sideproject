184fc2591495c21ef4b7848f808245d25f3b0177
Johnny Wright <johnnywrightiv@gmail.com>
2023-01-19 22:33:09 -0600

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductComponent from './containers/ProductComponent';
+import ProductDetails from './containers/ProductDetails';
+import NotFound from './containers/NotFound';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <BrowserRouter>
+        <h1>Hello</h1>
+        <Header />
+          <Routes>
+            <Route path="/" exact element={<ProductListing />}/>
+              <Route path="/product/:productID" exact element={<ProductDetails />}/>
+            <Route path="/*" element={<NotFound />}/>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+// <Route path="/" element={<ProductListing/>}></Route>
+
+// <Route path="/" exact element = {<ProductListing />} />
+//   <Route path="/product/:productId" exact element = {<ProductDetail />} />
